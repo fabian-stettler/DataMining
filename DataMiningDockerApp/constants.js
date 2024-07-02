320b16ed42ad436059edc8bf38e8b912847424ff
fabian-stettler <fabian.stettler@hispeed.ch>
2024-07-02 17:03:20 +0200

    Data Mining Code vom 02.07 Docker Container führt skript aus, Pfade stimmen jetzt.

--- DataMiningDockerApp/constants.js
+++ DataMiningDockerApp/constants.js
@@ -1,13 +1,13 @@
 const constants = {
     //File Paths
-    FILE_PATH_LOG_SUCCESSFULL: 'C:\\Users\\fabia\\Desktop\\htmlFiles\\logFile.txt',
-    FILE_PATH_LOG_ERROR: 'C:\\Users\\fabia\\Desktop\\htmlFiles\\logFileError.txt',
-    FILE_PATH_LOG_NO_ARTICLE: 'C:\\Users\\fabia\\Desktop\\htmlFiles\\logFileNoArticle.txt',
+    FILE_PATH_LOG_SUCCESSFULL: '/usr/src/datamining/logFiles/logFile.txt',
+    FILE_PATH_LOG_ERROR: '/usr/src/datamining/logFiles/logFileError.txt',
+    FILE_PATH_LOG_NO_ARTICLE: '/usr/src/datamining/logFiles/logFileNoArticle.txt',
     TARGET_DIR: "",
-    FILE_PATH_LOG_TEMP: 'C:\\Users\\fabia\\Desktop\\htmlFiles\\logFileTemp.txt',
-    OUTPUT_DIR: "C:\\Users\\fabia\\Desktop\\htmlFiles",
-    FILE_PATH_ALREADY_SAVED_ARTICLES: "C:\\Users\\fabia\\Desktop\\htmlFiles\\savedArticles.txt",
-    FILE_PATH_SUMMARY: "C:\\Users\\fabia\\Desktop\\htmlFiles\\logFileSummary",
+    FILE_PATH_LOG_TEMP: '/usr/src/datamining/logFiles/logFileTemp.txt',
+    OUTPUT_DIR: "/usr/src/datamining/htmlFiles",
+    FILE_PATH_ALREADY_SAVED_ARTICLES: "/usr/src/datamining/htmlFiles/savedArticles.txt",
+    FILE_PATH_SUMMARY: "/usr/src/datamining/logFiles/logFileSummary.txt",
 
     //other
     URLS_TO_IGNORE : ["https://www.srf.ch/meteo/wetter"],
